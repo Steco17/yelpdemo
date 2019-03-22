87ef754499f4c81e904a15762df9c95a51cf8668
steco17 <moyo.stephane@gmail.com>
2019-03-22 06:30:56 +0100

    link reviews to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_043012) do
+ActiveRecord::Schema.define(version: 2019_03_22_051638) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_03_22_043012) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "revoews", force: :cascade do |t|

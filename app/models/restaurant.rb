e2d93c0673f1bc07dc71e76807c2f3e8936518c4
steco17 <moyo.stephane@gmail.com>
2019-03-26 06:25:19 +0100

    add data validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,13 @@ class Restaurant < ApplicationRecord
      mount_uploader :image, ImageUploader
 
      has_many :reviews
+     
+     validates :name, :adress, :phone, :website, :image, presence: true
+     validates :phone, format: { with: /\d{3}/,
+    message: "must be in the format 000 000 000" }
+    validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+    message: "must start with http:// or https//:" }
+    validates :adress, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+    message: "must be in the formate 350 fifth avenue, New York, NT 10015" }
 end
+ 
